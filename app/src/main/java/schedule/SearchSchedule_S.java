571453bd0d781960ebd2beda2d8655e88d17bca7
aerhergag0 <aerhergag@naver.com>
2021-07-18 22:31:03 +0900

    0718

--- app/src/main/java/schedule/SearchSchedule_S.java
+++ app/src/main/java/schedule/SearchSchedule_S.java
@@ -21,7 +21,7 @@ public class SearchSchedule_S {
     searchLoop : while (true) {
       try {
         System.out.println("선택할 항목을 입력하여 주세요.\n");
-        System.out.println("1.제목으로 검색  2.날짜로 검색  3.내용으로 검색 4. 공유자로 검색(추가예정) 0.뒤로가기");
+        System.out.println("1.제목으로 검색  2.날짜로 검색  3.내용으로 검색 4. 공유자로 검색  0.뒤로가기");
         System.out.print("번호 입력 >> ");
         int menuNum = Integer.parseInt(sc.nextLine());
 
@@ -29,6 +29,7 @@ public class SearchSchedule_S {
           case 1 : this.search_s_title(id); break;
           case 2 : this.search_s_date(id); break;
           case 3 : this.search_s_contents(id); break;
+          case 4 : this.search_s_shares(id); break;
           case 0 : break searchLoop;
         }
       } catch(Exception e) {} // try-catch End
@@ -148,6 +149,42 @@ public class SearchSchedule_S {
       } // RS check End
     } catch (Exception e) {System.out.println(e);} // try-catch End
   } // search_contents Method End
+
+  public void search_s_shares(String id) {
+    banner.printbanner2("공유자로 검색");
+    try {
+      Boot boot = new Boot();
+      Connection CN = boot.boot();
+      Statement ST = CN.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+      ResultSet RS;
+      PreparedStatement PS;
+
+      // 게시물 존재 여부확인
+      msg = "select * from s_seet_"+id;
+      RS = ST.executeQuery(msg);
+      if (RS.next() == false) {
+        System.out.println("\n게시글이 존재하지 않습니다."); }
+      else {
+        while (true) {
+          System.out.print("검색할 내용을 입력하세요 (0: 뒤로가기) : ");
+          String keyword = sc.nextLine();
+          if (keyword.equals("0")) {return;}
+          msg = "select * from s_seet_"+id+" where shares = '"+keyword+"'";
+          RS = ST.executeQuery(msg);
+          RS.last();
+          int searchResultCount = RS.getRow();
+          RS.beforeFirst();
+          if(searchResultCount == 0) {
+            System.out.printf("검색하신 %s 에 대한 결과가 없습니다.\n\n", keyword);
+          }
+          else {
+            System.out.printf(" %s 님과 공유된 일정 총 %d 건의 검색 결과가 있습니다.\n\n",keyword, searchResultCount);
+            showtable.searchResultShowTable_S(id, "select rownum, a.s_date, a.title, a.contents, a.shares, a.s_id from (select * from s_seet_"+id+" order by s_date) a where shares = '"+keyword+"'");
+          }
+        } // search While end
+      } // RS check End
+    } catch (Exception e) {System.out.println(e);} // try-catch End
+  } // search_shares Method End
 } // Class End
 
 
